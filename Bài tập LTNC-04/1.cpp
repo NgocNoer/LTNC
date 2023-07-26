2ef412369999555a294febd2916c42c286391d85
ngocntn <91111075+NgocNoer@users.noreply.github.com>
2023-07-26 13:41:15 +0700

    Add files via upload

---
+++ Bài tập LTNC-04/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solve(int n, int p) {
+    int num_even = (p % 2 == 0) ? p / 2 : (p - 1) / 2;
+    int num_odd = (p % 2 == 0) ? (n - p + 1) / 2 : (n - p) / 2;
+    return max(num_even, num_odd);
+}
+
+int main() {
+    int q;
+    cin >> q;
+    while (q--) {
+        int n, p;
+        cin >> n >> p;
+        cout << solve(n, p) << endl;
+    }
+    return 0;
+}

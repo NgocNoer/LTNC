1b04a624a8bed3ba9cb17189479ba8b9bfecfa0e
ngocntn <91111075+NgocNoer@users.noreply.github.com>
2023-08-09 11:10:55 +0700

    Update 04

--- Bài tập LTNC-04/1.cpp
+++ Bài tập LTNC-04/1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int solve(int n, int p) {
     int num_even = (p % 2 == 0) ? p / 2 : (p - 1) / 2;
     int num_odd = (p % 2 == 0) ? (n - p + 1) / 2 : (n - p) / 2;
-    return max(num_even, num_odd);
+    return (num_even, num_odd);
 }
 
 int main() {

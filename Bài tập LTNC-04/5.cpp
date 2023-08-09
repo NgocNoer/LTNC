7bffcc1d51997e05b4036b90dac99e02620b600b
ngocntn <91111075+NgocNoer@users.noreply.github.com>
2023-08-09 11:13:57 +0700

    Update 5.cpp

--- Bài tập LTNC-04/5.cpp
+++ Bài tập LTNC-04/5.cpp
@@ -6,8 +6,9 @@ int main() {
     int n;
     cin >> n;
 
-    // Khởi tạo tập hợp để lưu trữ các phần tử của dãy A
+    // Khởi tạo tập hợp để lưu trữ các phần tử của dãy A và B
     set<int> A;
+    set<int> B;
 
     // Đọc dữ liệu vào dãy A và thêm các phần tử vào tập hợp
     for (int i = 0; i < n; i++) {
@@ -16,10 +17,15 @@ int main() {
         A.insert(x);
     }
 
-    // Duyệt qua dãy B và tìm phần tử duy nhất không có trong dãy A
+    // Đọc dữ liệu vào dãy B và thêm các phần tử vào tập hợp
     for (int i = 0; i < n+1; i++) {
         int x;
         cin >> x;
+        B.insert(x);
+    }
+
+    // Duyệt qua dãy B và tìm phần tử duy nhất không có trong dãy A
+    for (int i = 0; i < n+1; i++) {
         if (A.find(x) == A.end()) {
             cout << x << endl;
             break;

e1a94d43d58b91e5ca2329a118048982575d68c2
ngocntn <91111075+NgocNoer@users.noreply.github.com>
2023-08-09 11:16:29 +0700

    Update 3.cpp

--- Bài tập LTNC-06/3.cpp
+++ Bài tập LTNC-06/3.cpp
@@ -1,44 +1,45 @@
-#include <iostream>
-#include <string>
-using namespace std;
+    #include <iostream>
+    #include <string>
+    using namespace std;
 
-class Student {
-public:
-    string name;
-    int scores[5];
-    int totalScore;
+    class Student {
+    public:
+        string name;
+        int scores[5];
+        int totalScore;
 
-    void input() {
-        cin >> name;
-        for (int i = 0; i < 5; i++) {
-            cin >> scores[i];
+        void input() {
+            cin >> name;
+            for (int i = 0; i < 5; i++) {
+                cin >> scores[i];
+            }
         }
-    }
 
-    void calculateTotalScore() {
-        totalScore = 0;
-        for (int i = 0; i < 5; i++) {
-            totalScore += scores[i];
+        void calculateTotalScore() {
+            totalScore = 0;
+            for (int i = 0; i < 5; i++) {
+                totalScore += scores[i];
+            }
         }
-    }
-};
+    };
 
-int main() {
-    int n;
-    cin >> n;
-    Student students[100];
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        students[i].input();
-        students[i].calculateTotalScore();
-        sum += students[i].totalScore;
-    }
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if ((students[i].totalScore /n) > sum) {
-            count++;
+    int main() {
+        int n;
+        cin >> n;
+        Student students[100];
+        int sum;
+        cin >> sum;                 // Tổng điểm 5 môn của Kristen
+
+        for (int i = 0; i < n; i++) {
+            students[i].input();
+            students[i].calculateTotalScore();
         }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if ((students[i].totalScore) > sum) {
+                count++;
+            }
+        }
+        cout << count << endl;
+        return 0;
     }
-    cout << count << endl;
-    return 0;
-}
